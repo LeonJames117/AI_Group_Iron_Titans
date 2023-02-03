9a047781c9f69132b60c7025af15a6956f3bcb0f
Ben Scrafton <78149958+BenScrafton@users.noreply.github.com>
2023-02-03 14:52:28 +0000

    Beginning Controller

---
+++ Assets/Ben's Folder/Scripts/Character.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public float lookSpeed;
+
+    [SerializeField] public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }    
+}
