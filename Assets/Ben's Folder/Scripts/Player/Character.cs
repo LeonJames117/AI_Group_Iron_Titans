3d6105db4a2d6630ff9008ce0cc171f7336ee61f
Ben Scrafton <78149958+BenScrafton@users.noreply.github.com>
2023-02-24 12:48:27 +0000

    player edit

--- Assets/Ben's Folder/Scripts/Player/Character.cs
+++ Assets/Ben's Folder/Scripts/Player/Character.cs
@@ -4,14 +4,65 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed;
+    [SerializeField] public float walkSpeed;
+    [SerializeField] public float runSpeed;
+  
     [SerializeField] public float lookSpeed;
-
     [SerializeField] public float health;
 
+    Vector3 direction = Vector3.zero;
+
+    enum MoveState
+    {
+        IDLE,
+        WALKING,
+        RUNNING
+    }
+
+    MoveState moveState = MoveState.IDLE;
     // Start is called before the first frame update
     void Start()
     {
         
-    }    
+    }
+
+    private void Update()
+    {
+        switch (moveState)
+        {
+            case MoveState.IDLE:
+                break;
+            case MoveState.WALKING:
+                break;
+            case MoveState.RUNNING:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SetMoveDirection(Vector3 p_direction) 
+    {
+        direction = Vector3.Normalize(p_direction);
+    }
+
+    void ProcessIdle() 
+    {
+
+    }
+
+    void ProcessWalking() 
+    {
+
+    }
+
+    void ProcessRunning() 
+    {
+
+    } 
+
+    void Attack()
+    {
+
+    }
 }
